153963fefa65044559b08dbc752118abc11973c4
siyukw <siyukw@gmail.com>
2017-01-13 21:11:26 -0800

    face, neck, part of hair

--- src/Client.java
+++ src/Client.java
@@ -7,14 +7,17 @@ public class Client {
         String name = userInput.next();
         System.out.println("Hi " + name + ", what's your gender?('1' for male; '2' for female)");
         int gender = userInput.nextInt();
-        System.out.println("On a scale from 1 bald to 10 very long, how long is your hair?(integer only)");
+        System.out.println("On a scale from 1 bald to 4 very long, how long is your hair?(integer only)");
         int hairLength = userInput.nextInt();
+        System.out.println("On a scale from 1 very pale to 5 very dark, how light is your skin?(integer only)");
+        int skinColor = userInput.nextInt();
         System.out.println("On a scale from 1 blond to 10 black, how dark is your hair?(integer only)");
         int hairColor = userInput.nextInt();
-        System.out.println("Okay " + name + ", what is your eye color? (1 for blue, 2 for green, 3 for brown, 4 for black)");
+        System.out.println("d" +
+                "okay " + name + ", what is your eye color? (1 for blue, 2 for green, 3 for brown, 4 for black)");
         int eyeColor = userInput.nextInt();
 
-        MyFace mine = new MyFace(gender, hairLength, hairColor, eyeColor);
+        MyFace mine = new MyFace(gender, hairLength, hairColor, eyeColor, skinColor);
 
     }
 }
--- src/MyFace.java
+++ src/MyFace.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.*;
 
 public class MyFace {
     private DrawingPanel p;
@@ -6,28 +7,59 @@ public class MyFace {
     private int hairColor;
     private int eyeColor;
 
-    public MyFace(int gender, int hairLength, int hairColor, int eyeColor){
-        DrawingPanel p = new DrawingPanel(800, 1200);
+    public MyFace(int gender, int hairLength, int hairColor, int eyeColor, int skinColor){
+        DrawingPanel p = new DrawingPanel(800, 950);
+        Graphics g = p.getGraphics();
         if (gender == 1) {
-            p.setBackground(Color.BLUE);
+            p.setBackground(new Color(100,149,237));
+            g.setColor(new Color(0,0,139));
+            g.fillRect(150, 775, 500, 175);
         } else {
-            p.setBackground(Color.PINK);
+            p.setBackground(new Color(255,192,203));
+            g.setColor(new Color(255,20,147));
+            g.fillRect(150, 775, 500, 175);
         }
         this.hairColor = hairColor;
         this.hairLength = hairLength;
         this.eyeColor = eyeColor;
-        Graphics g = p.getGraphics();
 
-        // draw face and neck
+
+
+        hair(g, hairColor, hairLength);
+        skin(g, skinColor);
     }
 
+    // draw neck and face
+    public void skin(Graphics g, int skinColor){
+        if(skinColor == 1) {
+            g.setColor(new Color(255, 248, 220));
+        } else if (skinColor == 2) {
+            g.setColor(new Color(255, 222, 170));
+        } else if (skinColor == 3) {
+            g.setColor(new Color(250, 164, 96));
+        } else if (skinColor == 4) {
+            g.setColor(new Color(160, 82, 45));
+        } else {
+            g.setColor(new Color(90, 0, 0));
+
+        }
+
+        g.fillRect(370, 500, 60, 275);
+        g.fillOval(175, 120, 500, 550);
+        g.fillOval(370, 750, 60, 50);
+        g.fillOval(150, 320, 100, 120);
+        g.fillOval(600, 320, 100, 120);
+    }
     // draw eyes
     public void eyes(){
 
     }
 
     // draw hair
-    public void hair(){
+    public void hair(Graphics g, int hairColor, int hairLength){
+        if (hairLength == 2) {
+            g.fillOval(175, 100, 500, 440);
+        }
 
     }
 
